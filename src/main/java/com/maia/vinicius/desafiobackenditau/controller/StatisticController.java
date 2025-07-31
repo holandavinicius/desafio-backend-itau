26e2aa0b91ca9960e28054e8eafcef82b1ec8a7a
vinicius <viniciushm97@gmail.com>
2025-08-01 00:33:10 +0100

    feat: implement logging for all endpoints

--- src/main/java/com/maia/vinicius/desafiobackenditau/controller/StatisticController.java
+++ src/main/java/com/maia/vinicius/desafiobackenditau/controller/StatisticController.java
@@ -2,6 +2,8 @@ package com.maia.vinicius.desafiobackenditau.controller;
 
 import com.maia.vinicius.desafiobackenditau.dto.StatisticResponse;
 import com.maia.vinicius.desafiobackenditau.service.StatisticService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import java.util.DoubleSummaryStatistics;
 public class StatisticController {
 
     private final StatisticService statisticService;
+    private static final Logger log = LoggerFactory.getLogger(StatisticController.class);
 
     public StatisticController(StatisticService statisticService) {
         this.statisticService = statisticService;
@@ -19,8 +22,13 @@ public class StatisticController {
 
     @GetMapping("/statistics")
     public ResponseEntity<StatisticResponse> getStatistics(){
+        log.info("Requisição recebida para /statistics");
+
         DoubleSummaryStatistics stats = statisticService.getLastStatistics();
         StatisticResponse statisticResponse = new StatisticResponse(stats);
+
+        log.debug("Estatísticas calculadas: {}", stats);
+
         return ResponseEntity.ok().body(statisticResponse);
     }
 }
--- src/main/java/com/maia/vinicius/desafiobackenditau/controller/TransactionController.java
+++ src/main/java/com/maia/vinicius/desafiobackenditau/controller/TransactionController.java
@@ -5,6 +5,8 @@ import com.maia.vinicius.desafiobackenditau.exceptions.InvalidTransactionExcepti
 import com.maia.vinicius.desafiobackenditau.model.Transaction;
 import com.maia.vinicius.desafiobackenditau.service.TransactionService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,24 +16,35 @@ public class TransactionController {
 
     private final TransactionService transactionService;
 
+    private static final Logger log = LoggerFactory.getLogger(TransactionController.class);
+
     public TransactionController(TransactionService transactionService) {
         this.transactionService = transactionService;
     }
 
     @PostMapping("/transaction")
     public ResponseEntity<?> createTransaction(@Valid @RequestBody TransactionRequest request){
+        log.info("POST /transaction | New value received: {} | Timestamp: {}", request.getValue(), request.getDateTime());
+
         Transaction transaction = new Transaction(request.getValue(), request.getDateTime());
         try {
             transactionService.addTransaction(transaction);
+
+            log.info("New transaction created. value: {} | Timestamp: {}", request.getValue(), request.getDateTime());
             return ResponseEntity.status(HttpStatus.CREATED).build(); // 201 Created, sem corpo
         } catch (InvalidTransactionException e) {
+            log.info("Error creating transaction: {}", e.getMessage());
             return ResponseEntity.unprocessableEntity().build(); // 422 Unprocessable Entity
         }
     }
 
     @DeleteMapping("/transaction")
     public ResponseEntity<?> deleteTransactions(){
+        log.info("DELETE request received from /transaction");
+
         transactionService.deleteTransactions();
+
+        log.info("Transactions deleted");
         return ResponseEntity.ok().build();
     }
 }
